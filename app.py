e0085da6509b8818275bc453931fd3e735f307a5
Chinmayi Hegde <chinmayihegde13@gmail.com>
2024-04-04 11:11:25 -0700

    Added job pages

--- app.py
+++ app.py
@@ -5,44 +5,31 @@ app = Flask(__name__)
 
 with open('jobs.json', 'r') as jobs_file:
     jobs_data = json.load(jobs_file)
+    print(jobs_data.keys())
 
-# with open('users.json', 'r') as users_file:
-#     users_data = json.load(users_file)
+# @app.route('/')
+# def login(): 
+#     return render_template('index.html')
 
 @app.route('/')
-def login():
-    return render_template('index.html')
-
-@app.route('/search')
-def search():
-    return render_template('job-list.html')
+def search(): 
+    return render_template('job-list.html', jobs = jobs_data['jobs'])
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/user')
+@app.route('/user') 
 def user_profile():
     return f'Welcome, user!'
 
-@app.route('/login', methods=['POST'])
-def process_login():
-    email = request.form['email']
-    password = request.form['pswd']
-
-    return redirect(url_for('user_profile'))
-
-@app.route('/signup', methods=['POST'])
-def process_signup():
-    user_id = request.form['txt']
-    email = request.form['email']
-    password = request.form['pswd']
-    return email
-    # Store user data in the database (replace this with actual storage logic)
-    # users_db[user_id] = {'email': email, 'password': password}
-
-    # Redirect to the user's profile page upon successful registration
-    return redirect(url_for('user_profile', user_id=user_id))
+@app.route('/jobs/<int:job_id>') 
+def job_details(job_id):
+    job = next((job for job in jobs_data['jobs'] if job['job_id'] == job_id), None)
+    if job:
+        return render_template('job-details.html', job=job)
+    else:
+        return "Job not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
